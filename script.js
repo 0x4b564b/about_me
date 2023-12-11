9f0eb64ba55f2fe480d74ec322b75399599f3403
0x4b564b <106047512+0x4b564b@users.noreply.github.com>
2023-12-11 18:07:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputElement = document.getElementById('command-input');
+
+    inputElement.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent the default behavior of Enter key
+            handleCommand(inputElement.innerText);
+            inputElement.innerText = ''; // Clear the content for a new command
+        }
+    });
+});
+
+function handleCommand(command) {
+    const contentElement = document.querySelector('.content');
+    const newContent = document.createElement('p');
+    newContent.innerText = `$ ${command}`; // Display the command in the content area
+    switch (command.toLowerCase()) {
+        case 'help':
+            newContent.innerText = 'Available commands: help, about, projects';
+            break;
+        case 'about':
+            newContent.innerText = 'This is your portfolio. Customize it to showcase your skills and projects.';
+            break;
+        case 'projects':
+            newContent.innerText = 'List of your projects goes here.';
+            break;
+        default:
+            newContent.innerText = `Command not recognized: ${command}`;
+    }
+    contentElement.appendChild(newContent);
+    // Add logic here to handle the command as needed
+}
